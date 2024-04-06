6d340be3ca2f4b64b62822991854ab0b032bc292
Jacob Holland <jacobfholland@gmail.com>
2024-04-06 15:58:01 +0000

    [ADD] Example python

--- example/example.py
+++ example/example.py
@@ -1,6 +1,20 @@
-# run.py
-from medusa_logger.decorator import log
-from medusa_logger.logger import Logger
+# example/example.py
+import sys
+import os
+# Manual sys path update for package imports, so this file can be used
+# without PIP installing the package.
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+try:
+    from medusa_logger.logger import Logger
+    from medusa_logger.decorator import log
+except:
+    sys.exit(1)
+
+os.environ["MD_LOG_LEVEL"] = "DEBUG"  # Global log level filter
+
+# Functions defined with a @log decorator. Level or Condition may be
+# passed as arguments on the decorator, or omitted.
 
 
 @log
@@ -21,18 +35,19 @@ def condition_function(x, y):
     return x + y
 
 
+# Individual log statements
 logger = Logger(__name__)
 logger.critical("Test logging on the critical level")
 logger.warning("Test logging on the warning level")
 logger.info("Test logging on the info level")
 logger.debug("Test logging on the info level")
 
+# Calling functions that have @log decorator
+result = baseline_function(10, 2)
+result = level_function(x="foo", y="bar")
+result = condition_function(3, 6)
+
 try:
-    result = baseline_function(10, 2)
     result = baseline_function(10, 0)
 except ZeroDivisionError as e:
     logger.error(f"Caught an exception as expected: {e}")
-
-
-result = level_function(10, 2)
-result = condition_function(3, 6)
