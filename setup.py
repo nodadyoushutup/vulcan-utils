5fafbc9985e5aac715451c44c24542e7eaa3bdbc
nodadyoushutup <admin@nodadyoushutup.com>
2024-04-06 20:52:59 +0000

    [FIX] Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("docs/README.md", "r") as f:
 
 setup(
     name="vulcan-logger",
-    version="1.3.6",
+    version="1.3.7",
     description="A logging utility package with colored logs.",
     packages=find_packages(),
     install_requires=[
