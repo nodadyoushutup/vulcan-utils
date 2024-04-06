8989fa87083c01816c0f7e7da50ca62aad209725
nodadyoushutup <admin@nodadyoushutup.com>
2024-04-06 21:41:40 +0000

    [IMP] Reviewer on Renovate bot

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("docs/README.md", "r") as f:
 
 setup(
     name="vulcan-logger",
-    version="1.3.10",
+    version="1.3.11",
     description="A logging utility package with colored logs.",
     packages=find_packages(),
     install_requires=[
