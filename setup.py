2e1d32eb04662e6c60cc9d206da439ead19599f2
nodadyoushutup <admin@nodadyoushutup.com>
2024-04-08 01:14:27 +0000

    [IMP] Private logger

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("docs/README.md", "r") as f:
 
 setup(
     name="vulcan-logger",
-    version="1.5.1",
+    version="1.5.2",
     description="A logging utility package with colored logs.",
     packages=find_packages(),
     install_requires=[
