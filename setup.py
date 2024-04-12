a0e3a4e2f51a480e4a011253af5fd7aa58ac3639
nodadyoushutup <admin@nodadyoushutup.com>
2024-04-12 21:03:19 +0000

    VUTIL-51 [REL] Release 1.11.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("docs/README.md", "r") as f:
 
 setup(
     name="vulcan-utils",
-    version="1.11.4",
+    version="1.11.5",
     description="A utility package for Python",
     packages=find_packages(),
     install_requires=[
