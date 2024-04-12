2cb77116ec5e223f8eed708eaa67557cb95fb427
nodadyoushutup <admin@nodadyoushutup.com>
2024-04-12 16:53:14 +0000

    VUTIL-43 [REL] Release 1.11.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("docs/README.md", "r") as f:
 
 setup(
     name="vulcan-logger",
-    version="1.10.2",
+    version="1.11.0",
     description="A logging utility package with colored logs.",
     packages=find_packages(),
     install_requires=[
