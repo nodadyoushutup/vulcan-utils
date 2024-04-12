50612ea3128be5ba4eaf90b8066337f65d8f3a3e
nodadyoushutup <admin@nodadyoushutup.com>
2024-04-12 01:34:19 +0000

    VLOG-25 [REL] Release 1.7.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("docs/README.md", "r") as f:
 
 setup(
     name="vulcan-logger",
-    version="1.6.0",
+    version="1.7.0",
     description="A logging utility package with colored logs.",
     packages=find_packages(),
     install_requires=[
